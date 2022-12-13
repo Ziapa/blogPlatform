8633094bc7a1bd6d37a0b108faaa61b8f242d888
Ziapa <60856599+Ziapa@users.noreply.github.com>
2022-12-13 19:45:54 +0300

    Delete dist directory
    deleted dist

--- dist/router/video-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.blogsRouter = void 0;
-const express_1 = require("express");
-const blogs_repositories_1 = require("../repositories/blogs-repositories");
-exports.blogsRouter = (0, express_1.Router)();
-exports.blogsRouter.get('/', (req, res) => {
-    res.status(200).send(blogs_repositories_1.blogsRepositories.findBlog());
-});
