dc0ea1b8456c78b4d48a93de17f5b9e38e2b2046
Ziapa <SmiT4eggg@bk.ru>
2022-12-19 10:08:20 +0300

    fix validator

--- src/router/blogs-router.ts
+++ src/router/blogs-router.ts
@@ -5,38 +5,36 @@ import {RequestWithBody, RequestWithParams, RequestWithParamsAndBody} from "../t
 import {CreateBlogsModel} from "../model/blogsModel/createBlogsModel";
 import {QueryBlogsModelType} from "../model/blogsModel/queryBlogsModel";
 import {body, validationResult} from "express-validator";
+import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 
 
 export const blogsRouter = Router()
 
-blogsRouter.get('/',
-    (req: Request, res: Response<blogsType[] | blogsType>) => {
+blogsRouter.get('/', (req: Request, res: Response<blogsType[] | blogsType>) => {
 
-        const findBlogs = blogsRepositories.findBlog()
+    const findBlogs = blogsRepositories.findBlog()
 
-        if (findBlogs) {
-            res.status(200).send(findBlogs)
-        }
+    if (findBlogs) {
+        res.status(200).send(findBlogs)
+    }
 
-    })
-blogsRouter.get('/:id',
-    (req: RequestWithParams<QueryBlogsModelType>, res: Response<blogsType[] | blogsType>) => {
-        res.status(200).send(blogsRepositories.findBlog(req.params.id))
-    })
-
-
+})
+blogsRouter.get('/:id', (req: RequestWithParams<QueryBlogsModelType>, res: Response<blogsType[] | blogsType>) => {
+    res.status(200).send(blogsRepositories.findBlog(req.params.id))
+})
 blogsRouter.post('/',
 
-    body('name').isString,
-    body('description').isString,
-    body('websiteUrl').isString,
+    body("name").isString().isEmpty(),
+    body("description").isString().isEmpty(),
+    body("websiteUrl").isString().isEmpty(),
+    inputValidationMiddleware,
 
-(req: RequestWithBody<CreateBlogsModel>, res: Response) => {
+    (req: RequestWithBody<CreateBlogsModel>, res: Response) => {
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});
-    }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
     const newBlog = blogsRepositories.crateBlog(req.body)
     if (newBlog) {
@@ -44,8 +42,7 @@ blogsRouter.post('/',
     } else {
         res.sendStatus(404)
     }
-}
-)
+})
 blogsRouter.put('/:id', (req: RequestWithParamsAndBody<QueryBlogsModelType, CreateBlogsModel>, res: Response) => {
     const updateBlog = blogsRepositories.updateBlog(req.params.id, req.body)
     if (updateBlog) {
@@ -54,11 +51,11 @@ blogsRouter.put('/:id', (req: RequestWithParamsAndBody<QueryBlogsModelType, Crea
         res.sendStatus(400)
     }
 })
-blogsRouter.delete('/:id', (req: Request, res: Response) => {
+blogsRouter.delete('/', (req: Request, res: Response) => {
     if (blogsRepositories.deletedBlog(req.params.id)) {
         res.sendStatus(204)
-    } else {
+    } else  {
         res.sendStatus(404)
     }
-})
+} )
 
--- src/router/posts-router.ts
+++ src/router/posts-router.ts
@@ -1,6 +1,6 @@
 import {Request, Response, Router} from "express";
 import {postsRepositories} from "../repositories/posts-repositories";
-import {body, validationResult} from "express-validator";
+import {body} from "express-validator";
 
 
 export const postsRouter = Router()
@@ -14,18 +14,11 @@ postsRouter.get('/:id', (req: Request, res: Response) => {
 
 postsRouter.post('/',
 
-    body('title').isString().isLength({min: 1, max: 30}).trim(),
-    body('shortDescription').isString().isLength({min: 1, max: 100}).trim(),
-    body('content').isString().isLength({min: 1, max: 1000}).trim(),
-    body('blogId').isString().trim(),
-
+    body("name").isString(),
+    body("description").trim().isString(),
+    body("websiteUrl").trim().isString(),
 
     (req: Request, res: Response) => {
-
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({errors: errors.array()});
-        }
 
     const newPost = postsRepositories.createPost(req.body)
     if (newPost) {
@@ -44,7 +37,7 @@ postsRouter.put('/:id', (req: Request, res: Response) => {
     }
 
 })
-postsRouter.delete('/:id', (req: Request, res: Response) => {
+postsRouter.delete('/', (req: Request, res: Response) => {
     if (postsRepositories.deletePost(req.params.id)) {
         res.sendStatus(204)
     } else {
