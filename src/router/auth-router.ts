c8b40df52a803b6e18ce6fa5f293a8efa8a93701
Ziapa <SmiT4eggg@bk.ru>
2023-01-18 07:26:08 +0300

    add auth login

---
+++ src/router/auth-router.ts
@@ -0,0 +1,21 @@
+import {Response, Router} from "express";
+import {usersServices} from "../domain/users-sevices";
+import {RequestWithBody} from "../types/types";
+import {UserAuthRequest} from "../types/usersType";
+
+export const authRouter = Router()
+
+authRouter.post("/",
+
+
+
+    async (req: RequestWithBody<UserAuthRequest>, res: Response<boolean> ) => {
+
+        const result = await usersServices.checkCredentials(req.body)
+        if (result) {
+            res.sendStatus(204)
+        } else {
+            res.sendStatus(400)
+        }
+        return res
+    })
