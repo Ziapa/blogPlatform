3853f0c1865656dd97a16726891a24c04e726b69
Ziapa <SmiT4eggg@bk.ru>
2023-01-27 16:53:45 +0300

    fix request post comments and add validator content

--- src/repositories/comments/comments-repositories.ts
+++ src/repositories/comments/comments-repositories.ts
@@ -1,15 +1,23 @@
 import {commentsCollection} from "../db";
-import {CommentsDbType, CommentsOutputType} from "../../types/commentsType";
+import {CommentsDbType} from "../../types/commentsType";
 
 
 export const commentsRepositories = {
 
-    async crateBlog(newComments: CommentsDbType): Promise<CommentsOutputType | null> {
+    async crateBlog(newComments: CommentsDbType): Promise<CommentsDbType | null> {
 
         const result = await commentsCollection.insertOne(newComments)
 
         if (result.acknowledged) {
-            return {content: newComments.content}
+            return {
+                id: newComments.id,
+                content: newComments.content,
+                commentatorInfo: {
+                    userId: newComments.commentatorInfo.userId,
+                    userLogin: newComments.commentatorInfo.userLogin
+                },
+                createdAt: newComments.createdAt
+            }
         } else {
             return null
         }
